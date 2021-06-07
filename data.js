f0998be83e7236eaed8b2195ae2f553a1140bb02
Smolnikov Vitaly <vvs1984@bk.ru>
2021-06-07 15:03:01 +0300

    add index pan + church + fiat 500

---
+++ data.js
@@ -0,0 +1,49 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0--",
+      "name": "Будем знакомы",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "yaw": 0.35331838871784527,
+        "pitch": -0.06883150607898258,
+        "fov": 1.3729094996294073
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.441525582756908,
+          "pitch": -0.143203035700445,
+          "title": "Обо мне<br>",
+          "text": "Био + информация о панорамной съемке<br>"
+        }
+      ]
+    }
+  ],
+  "name": "Обо мне",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
