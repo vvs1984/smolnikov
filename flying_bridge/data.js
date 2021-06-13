ef0e17001d4b6322c9882cd1d66a5309ab4bf83f
Smolnikov Vitaly <vvs1984@bk.ru>
2021-06-13 19:41:27 +0300

    add flying bridge

---
+++ flying_bridge/data.js
@@ -0,0 +1,42 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-flying-bridge",
+      "name": "Flying Bridge",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "yaw": -0.9224136758408008,
+        "pitch": 0.14687515900716264,
+        "fov": 1.35820747241737
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "flying_bridge",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
