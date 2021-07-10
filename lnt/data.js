199fa166f84bc6628a93ba4efd7719027b83f0a5
Smolnikov Vitaly <vvs1984@bk.ru>
2021-07-10 09:56:06 +0300

    lnt_draft

---
+++ lnt/data.js
@@ -0,0 +1,747 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1----",
+      "name": "1 зал Почта в Туле",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "yaw": -3.1086269114930314,
+        "pitch": 0.1827574258944864,
+        "fov": 1.35820747241737
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5830834598064172,
+          "pitch": 0.5582145583506719,
+          "rotation": 0,
+          "target": "1-1-----"
+        },
+        {
+          "yaw": -1.1993274951769237,
+          "pitch": 0.676213843426698,
+          "rotation": 0,
+          "target": "11-1---"
+        },
+        {
+          "yaw": -2.472273767438944,
+          "pitch": 0.5648647189147056,
+          "rotation": 10.995574287564278,
+          "target": "4-------"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-1-----",
+      "name": "1 зал Слова сказанные в Телефон",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "yaw": -3.115188954649355,
+        "pitch": 0.12330356695948907,
+        "fov": 1.35820747241737
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6092115032332757,
+          "pitch": 0.5701173384329774,
+          "rotation": 0,
+          "target": "0-1----"
+        },
+        {
+          "yaw": 1.7789393065269072,
+          "pitch": 0.5999288862846672,
+          "rotation": 0,
+          "target": "2-1----"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-1----",
+      "name": "1 зал Кино Музеи Библиотеки",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "yaw": 1.817643939699984,
+        "pitch": 0.3621947447466347,
+        "fov": 1.35820747241737
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6004144099046549,
+          "pitch": 0.6333288437898936,
+          "rotation": 0,
+          "target": "1-1-----"
+        },
+        {
+          "yaw": 0.27377391740254353,
+          "pitch": 0.5852642777780339,
+          "rotation": 0,
+          "target": "3-1------"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-1------",
+      "name": "1 зал Дай Бог досуга и желания",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "yaw": 2.8718958483608077,
+        "pitch": 0.11198425555411085,
+        "fov": 1.35820747241737
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.808400204852882,
+          "pitch": 0.5186386073567526,
+          "rotation": 0,
+          "target": "2-1----"
+        },
+        {
+          "yaw": -1.1576131078756475,
+          "pitch": 0.3983957413423056,
+          "rotation": 0,
+          "target": "1-1-----"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-------",
+      "name": "История железной дороги и авто в Туле",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "yaw": 0.41082365470020754,
+        "pitch": -0.11209415178134563,
+        "fov": 1.35820747241737
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7340409001044028,
+          "pitch": 0.3248058025578757,
+          "rotation": 0,
+          "target": "11-1---"
+        },
+        {
+          "yaw": -1.685198025090891,
+          "pitch": 0.4735309722298986,
+          "rotation": 0,
+          "target": "6-2--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-2--",
+      "name": "2 зал Доктора",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "yaw": -3.035777894037677,
+        "pitch": 0.15164750897715962,
+        "fov": 1.35820747241737
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0272772878343055,
+          "pitch": 0.6480132351063084,
+          "rotation": 0,
+          "target": "6-2--"
+        },
+        {
+          "yaw": 2.434426071686328,
+          "pitch": 0.47418565677637403,
+          "rotation": 1.5707963267948966,
+          "target": "4-------"
+        },
+        {
+          "yaw": -1.731460659943238,
+          "pitch": 0.6706663302669131,
+          "rotation": 0,
+          "target": "10-2--"
+        },
+        {
+          "yaw": -1.6755467166249556,
+          "pitch": 0.3440012496853768,
+          "rotation": 0,
+          "target": "8-2----"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-2--",
+      "name": "2 зал вход",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "yaw": 0.5215067000564471,
+        "pitch": 0.20363770906945078,
+        "fov": 1.35820747241737
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5920110853557432,
+          "pitch": 0.4165899722950499,
+          "rotation": 0,
+          "target": "4-------"
+        },
+        {
+          "yaw": 1.6365298414255856,
+          "pitch": 0.428008939610935,
+          "rotation": 0,
+          "target": "7-2--"
+        },
+        {
+          "yaw": 0.0629786477837353,
+          "pitch": 0.6729066072889545,
+          "rotation": 0,
+          "target": "5-2--"
+        },
+        {
+          "yaw": 0.06078801068294837,
+          "pitch": 0.45904237084990385,
+          "rotation": 0,
+          "target": "10-2--"
+        },
+        {
+          "yaw": 0.054070686652423205,
+          "pitch": 0.2618904889757374,
+          "rotation": 0,
+          "target": "8-2----"
+        },
+        {
+          "yaw": 0.5476058436634546,
+          "pitch": 0.19904788637431992,
+          "rotation": 0,
+          "target": "13-2---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-2--",
+      "name": "2 зал Записки",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "yaw": -1.1277512089809552,
+        "pitch": 0.1737459352610955,
+        "fov": 1.35820747241737
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6461462677450704,
+          "pitch": 0.3999961201449427,
+          "rotation": 0,
+          "target": "6-2--"
+        },
+        {
+          "yaw": 2.355713448115794,
+          "pitch": 0.8330188652536616,
+          "rotation": 0,
+          "target": "12-2---"
+        },
+        {
+          "yaw": 2.3623650247257757,
+          "pitch": 0.28732168471543496,
+          "rotation": 0,
+          "target": "13-2---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-2----",
+      "name": "2 зал Жизнь есть Движение",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "yaw": 2.520448825296887,
+        "pitch": 0.28209373475545263,
+        "fov": 1.35820747241737
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8103199389770204,
+          "pitch": 0.26995054649676753,
+          "rotation": 0,
+          "target": "6-2--"
+        },
+        {
+          "yaw": 0.8730410452235837,
+          "pitch": 0.706971038207671,
+          "rotation": 0,
+          "target": "10-2--"
+        },
+        {
+          "yaw": 0.8464221913424002,
+          "pitch": 0.47501031962886664,
+          "rotation": 0,
+          "target": "5-2--"
+        },
+        {
+          "yaw": -1.0048040284879711,
+          "pitch": 0.7337291883690735,
+          "rotation": 0,
+          "target": "9-2--1991"
+        },
+        {
+          "yaw": -0.927220633346197,
+          "pitch": 0.44235368318365076,
+          "rotation": 0,
+          "target": "13-2---"
+        },
+        {
+          "yaw": -0.019924248377627407,
+          "pitch": 0.3242037213843556,
+          "rotation": 0.7853981633974483,
+          "target": "12-2---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-2--1991",
+      "name": "2 зал 1991",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "yaw": -0.14231146208568646,
+        "pitch": -0.1513271049048157,
+        "fov": 1.35820747241737
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.647446443619609,
+          "pitch": 0.6551994567637536,
+          "rotation": 0,
+          "target": "8-2----"
+        },
+        {
+          "yaw": 1.3672915870314775,
+          "pitch": 0.7492866100884505,
+          "rotation": 0,
+          "target": "13-2---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-2--",
+      "name": "2 зал ТГПУ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "yaw": 3.1275779915257313,
+        "pitch": 0.09860790257285679,
+        "fov": 1.35820747241737
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.661077409621944,
+          "pitch": 0.6753216799885173,
+          "rotation": 0,
+          "target": "5-2--"
+        },
+        {
+          "yaw": 1.6198901015978535,
+          "pitch": 0.3774973528010346,
+          "rotation": 0,
+          "target": "6-2--"
+        },
+        {
+          "yaw": -1.672185178622307,
+          "pitch": 0.6109057801903877,
+          "rotation": 0,
+          "target": "8-2----"
+        },
+        {
+          "yaw": -0.9684963568892986,
+          "pitch": 0.5199363724110437,
+          "rotation": 0.7853981633974483,
+          "target": "9-2--1991"
+        },
+        {
+          "yaw": -0.6427639394747189,
+          "pitch": 0.3717809111699033,
+          "rotation": 0.7853981633974483,
+          "target": "13-2---"
+        },
+        {
+          "yaw": 0.23514758662656732,
+          "pitch": 0.430154276453651,
+          "rotation": 1.5707963267948966,
+          "target": "12-2---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-1---",
+      "name": "1 зал Писание моё",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.35820747241737
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.3797291350156122,
+          "pitch": 0.6696207352818533,
+          "rotation": 0,
+          "target": "0-1----"
+        },
+        {
+          "yaw": -2.775986730647528,
+          "pitch": 0.4364647633426184,
+          "rotation": 0,
+          "target": "1-1-----"
+        },
+        {
+          "yaw": -1.615406363574074,
+          "pitch": 0.6043831595284495,
+          "rotation": 0,
+          "target": "4-------"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-2---",
+      "name": "2 зал о покупках",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "yaw": 0.39874060603254513,
+        "pitch": 0.21484712424759067,
+        "fov": 1.35820747241737
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9549676962162081,
+          "pitch": 0.3372120495605735,
+          "rotation": 0,
+          "target": "13-2---"
+        },
+        {
+          "yaw": -1.1654610998143706,
+          "pitch": 0.7320741024698183,
+          "rotation": 0,
+          "target": "7-2--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-2---",
+      "name": "2 зал Духовная сила",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "yaw": 0.8511836505880179,
+        "pitch": -0.024287066219301323,
+        "fov": 1.35820747241737
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.614651127354069,
+          "pitch": 0.8466146496145122,
+          "rotation": 0,
+          "target": "9-2--1991"
+        },
+        {
+          "yaw": -1.7190974506527112,
+          "pitch": 0.5072399561639216,
+          "rotation": 0,
+          "target": "8-2----"
+        },
+        {
+          "yaw": -2.9619587688573823,
+          "pitch": 0.2801321106232564,
+          "rotation": 0,
+          "target": "6-2--"
+        },
+        {
+          "yaw": 2.749307699359374,
+          "pitch": 0.48579894173493443,
+          "rotation": 0,
+          "target": "12-2---"
+        },
+        {
+          "yaw": 2.725020927236657,
+          "pitch": 0.2875297850806806,
+          "rotation": 0,
+          "target": "7-2--"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "LNT",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
