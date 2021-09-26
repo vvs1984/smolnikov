b2c97e4a16a31be37771c3aa0c87f0a783d5e99a
Smolnikov Vitaly <vvs1984@bk.ru>
2021-09-26 19:33:59 +0300

    new design + file managment
    virtual tours goes to tours folder
    add portfolio foto (report+ interior + sport)
    new design (from virtual tour page to classic)

---
+++ tours/church/data.js
@@ -0,0 +1,42 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0----",
+      "name": "Церковь-часовня Архангела Михаила",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "yaw": 0.001869671107828097,
+        "pitch": 0.06139137741892142,
+        "fov": 1.3729094996294073
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "church",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
---
+++ tours/ostashevo/data.js
@@ -0,0 +1,275 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "Фасад",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "yaw": -0.25642914394686933,
+        "pitch": -0.14324549622795146,
+        "fov": 1.3729094996294073
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.22835007162424148,
+          "pitch": 0.11712652447621785,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-",
+      "name": "Вход",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "yaw": 0.03087890215067013,
+        "pitch": 0.19904720247228802,
+        "fov": 1.3729094996294073
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.043818178707530464,
+          "pitch": 0.3523480380642052,
+          "rotation": 0,
+          "target": "2-1-"
+        },
+        {
+          "yaw": -3.1397678254368238,
+          "pitch": 0.1773846585269574,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-1-",
+      "name": "1 этаж",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "yaw": 0.04698848621953822,
+        "pitch": 0.0018603109751076374,
+        "fov": 1.3729094996294073
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.01369069042410942,
+          "pitch": 0.024049099031064003,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": -3.0486084205247064,
+          "pitch": 0.4396738322508522,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": -3.0823684940095166,
+          "pitch": 0.11386874984300377,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-",
+      "name": "Лестница",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.024624670838298002,
+          "pitch": 0.5692761352707674,
+          "rotation": 0,
+          "target": "2-1-"
+        },
+        {
+          "yaw": 0.02247691707773214,
+          "pitch": 0.36121172772220156,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": 0.005960286016412297,
+          "pitch": -0.12152773823960672,
+          "rotation": 0,
+          "target": "4-2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-2-",
+      "name": "2 этаж",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "yaw": -3.067055377507238,
+        "pitch": 0.07373183742804201,
+        "fov": 1.3729094996294073
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0896400583417094,
+          "pitch": 0.26450678238930614,
+          "rotation": 0,
+          "target": "5-"
+        },
+        {
+          "yaw": 0.016959762633398157,
+          "pitch": 0.5547890497366375,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-",
+      "name": "Зал",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "yaw": 0.026744104189400986,
+        "pitch": 0.13802152425918024,
+        "fov": 1.3729094996294073
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.024421516480577665,
+          "pitch": 0.3234358937241293,
+          "rotation": 0,
+          "target": "4-2-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "ostashevo",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
