50605da6e3692017ef52eff206d97492772e3fa1
Smolnikov Vitaly <vvs1984@bk.ru>
2021-09-28 14:06:47 +0300

    add_gosvetaltufevo_in

---
+++ tours/gos_vet/gosvet_altufevo_in/data.js
@@ -0,0 +1,42 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-_360",
+      "name": "вход_360",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "yaw": 1.1788322429661804,
+        "pitch": 0.2368889417091573,
+        "fov": 1.35820747241737
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
