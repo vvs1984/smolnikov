52ef90a05bb0d863d9b4fd729b62fa82b7617252
Smolnikov Vitaly <vvs1984@bk.ru>
2021-09-28 10:56:37 +0300

    add gosvet altufevo demo

---
+++ tours/gos_vet/gosvet_avtufevo_v1/data.js
@@ -0,0 +1,219 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "Вход",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "yaw": -0.6041524333826587,
+        "pitch": 0.28584008704246244,
+        "fov": 1.35820747241737
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1944178374240781,
+          "pitch": 0.23982530994427265,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": 2.2019277896016227,
+          "pitch": 0.27898117654975785,
+          "rotation": 0,
+          "target": "2---"
+        },
+        {
+          "yaw": 0.6604868321405508,
+          "pitch": 0.41671801471910896,
+          "rotation": 5.497787143782138,
+          "target": "1--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1--",
+      "name": "Приемный кабинет",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "yaw": 0.2953814566843871,
+        "pitch": 0.2260816496137501,
+        "fov": 1.35820747241737
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.605385905491575,
+          "pitch": 0.4142977191771511,
+          "rotation": 7.853981633974483,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2---",
+      "name": "Кабинет диагностики УЗИ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "yaw": 2.4231734887450447,
+        "pitch": 0.4369596101847151,
+        "fov": 1.35820747241737
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9889494260480962,
+          "pitch": 0.3624377376811019,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-",
+      "name": "Рентген",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "yaw": 0.9612736495599634,
+        "pitch": 0.2951812663575488,
+        "fov": 1.35820747241737
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.06453853138207499,
+          "pitch": 0.35408473882706204,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": 3.1089419985019955,
+          "pitch": 0.23062470929233925,
+          "rotation": 4.71238898038469,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-",
+      "name": "Операционная",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "yaw": 0.2972304786409108,
+        "pitch": 0.3542556920490689,
+        "fov": 1.35820747241737
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0526612736957048,
+          "pitch": 0.14976768008797947,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "GosVet_Avtufevo_V1",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
