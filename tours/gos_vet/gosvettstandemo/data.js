aca8825ee4c70835c81dc38dc3b0625573874ec1
Smolnikov Vitaly <vvs1984@bk.ru>
2021-09-29 09:38:25 +0300

    add gosvettstandemo

---
+++ tours/gos_vet/gosvettstandemo/data.js
@@ -0,0 +1,175 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "Вход",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "yaw": 0.335040883195326,
+        "pitch": 0.07662967273867949,
+        "fov": 1.505669515703224
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.533174365990286,
+          "pitch": 0.48200779592267295,
+          "rotation": 0,
+          "target": "1--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1--",
+      "name": "Кабинет приёма",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "yaw": -0.313224267545408,
+        "pitch": 0.22160561402029622,
+        "fov": 1.505669515703224
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7613811049316563,
+          "pitch": 0.5153846469553631,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 1.6096757974349174,
+          "pitch": 0.2963819402679899,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-",
+      "name": "Груминг",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "yaw": -3.0644778750455224,
+        "pitch": 0.38875692453222754,
+        "fov": 1.505669515703224
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.181882140280301,
+          "pitch": 0.30975144921967157,
+          "rotation": 0,
+          "target": "1--"
+        },
+        {
+          "yaw": -1.34406128096116,
+          "pitch": 0.4313106526427557,
+          "rotation": 0,
+          "target": "3--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3--",
+      "name": "Диагностический кабинет",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "yaw": -0.08570820795918976,
+        "pitch": 0.35415345212917515,
+        "fov": 1.505669515703224
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.636883758719378,
+          "pitch": 0.446846030188075,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "GosVetTStanDemo",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
